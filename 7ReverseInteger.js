ccea38d8d766b69d873d768dbebeebef6c251156
sakibahammed <sakib.ahammed2345@gmail.com>
2023-07-30 22:38:04 +0600

    wrong code / this code has error

---
+++ 7ReverseInteger.js
@@ -0,0 +1,18 @@
+const MIN = -2147483648;
+const MAX = 2147483648;
+
+var reverse = function(x) {
+    
+  let reversed = parseInt(x.toString().split('').reverse().join(''));
+  if(x<0){
+    reversed *-1
+  }
+  if(reversed<MIN || reversed>MAX){
+    return 0
+  }
+  return reversed
+ 
+};  
+
+
+console.log(reverse(-12));
