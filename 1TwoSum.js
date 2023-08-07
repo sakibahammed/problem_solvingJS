db0262e7b9a4abdfeed0ec7f5f57b90b56663523
sakibahammed <sakib.ahammed2345@gmail.com>
2023-08-08 01:54:38 +0600

    twosum.js

---
+++ 1TwoSum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+   
+
+    for(let i= 0;i<nums.length;i++){
+        for(let j= i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+                return [i,j];
+            }
+        }
+    }
+};
+
+console.log(twoSum([1,2,3,4],5))
