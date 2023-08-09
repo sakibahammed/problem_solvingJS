c8353dbe9092688bdbcda9f23e1939a803f418e2
sakibahammed <sakib.ahammed2345@gmail.com>
2023-08-09 16:19:19 +0600

    1046,th test case failed - all good

--- 4MedianofTwoSortedArrays.js
+++ 4MedianofTwoSortedArrays.js
@@ -1,8 +1,18 @@
 var findMedianSortedArrays = function(nums1, nums2) {
-  let mergedArray = [...nums1,...nums2].sort() ;
-
-  let median = (mergedArray[0] + mergedArray[mergedArray.length-1]/2)
-  return median;
+  let mergerdArray = [...nums1 , ...nums2].sort();
+  let start= 0;
+  let end = mergerdArray.length - 1;
+  if(mergerdArray.length %2==0){
+      let midNumber = Math.floor((start + end)/2)
+      let secondMid = midNumber+1;
+      let Median = (mergerdArray[midNumber] + mergerdArray[secondMid])/2
+      return Median;
+  }
+  else{
+      let MedianOdd = (mergerdArray[0] + mergerdArray[mergerdArray.length-1])/2
+      // if((mergerdArray[0]||mergerdArray[mergerdArray.length-1]<0)){
+          // MedianOdd = MedianOdd*(-1)
+      // }
+      return MedianOdd
+  }
 };
-
-console.log(findMedianSortedArrays([1,3],[2]));
