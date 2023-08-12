0f46440e1abd1107db2cd33b454001f669810d0a
sakibahammed <sakib.ahammed2345@gmail.com>
2023-08-13 04:24:36 +0600

    makeArray in a measerable way

---
+++ 2659MakeArrayEmpty.js
@@ -0,0 +1,12 @@
+// var countOperationsToEmptyArray = function(nums) {
+//     let counter = 0;
+//     let newArray = [];
+//   for(let i=0;i<nums.length;i++){
+//     newArray.push(nums[i]);
+//     counter++;
+//     newArray.pop()
+//   }
+//   return counter;
+// };
+
+// console.log(countOperationsToEmptyArray([3,4,1]));
