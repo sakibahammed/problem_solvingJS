39cfef09cabc261f4a37ed37eb0af83e39a4aeca
sakibahammed <sakib.ahammed2345@gmail.com>
2023-08-13 04:35:35 +0600

    wromg code

--- 2659MakeArrayEmpty.js
+++ 2659MakeArrayEmpty.js
@@ -1,12 +1,38 @@
-// var countOperationsToEmptyArray = function(nums) {
-//     let counter = 0;
-//     let newArray = [];
-//   for(let i=0;i<nums.length;i++){
-//     newArray.push(nums[i]);
-//     counter++;
-//     newArray.pop()
-//   }
-//   return counter;
-// };
+// // var countOperationsToEmptyArray = function(nums) {
+// //     let counter = 0;
+// //     let newArray = [];
+// //   for(let i=0;i<nums.length;i++){
+// //     newArray.push(nums[i]);
+// //     counter++;
+// //     newArray.pop()
+// //   }
+// //   return counter;
+// // };
 
-// console.log(countOperationsToEmptyArray([3,4,1]));
+// // console.log(countOperationsToEmptyArray([3,4,1]));
+
+
+
+//  var countOperationsToEmptyArray = function(nums) {   
+    
+//         let newArray = [];
+//         let counter = 0;
+
+
+//         for(let i=0;i<nums.length;i++){
+//             counter++
+//             newArray.push(nums[i]);
+            
+//             newArray.sort();
+//             // counter++;
+//             newArray.shift()
+//             counter++
+            
+
+//         }
+
+//         return counter
+
+
+// }
+// console.log(countOperationsToEmptyArray([3,4,-1]));
