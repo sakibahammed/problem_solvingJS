8ce3b624dd6e9032e54dce809a6571b23a0ad264
sakibahammed <sakib.ahammed2345@gmail.com>
2023-08-30 13:55:50 +0600

    single number solved using Xor

--- 136SingleNumberAi.js
+++ 136SingleNumberAi.js
@@ -1,27 +1,41 @@
-// var singleNumber = function(nums) {
-//     let value = 0;
-//     for(let num of nums){
-//         value ^=num; 
+// // var singleNumber = function(nums) {
+// //     let value = 0;
+// //     for(let num of nums){
+// //         value ^=num; 
+// //     }
+
+// //     return value;
+// // };
+
+// // console.log(singleNumber([1, 2, 2, 3, 3, 3, 4, 4, 1, 5]));
+
+
+// function findSingleValue(nums) {
+//     let result = 0;
+    
+//     for (const num of nums) {
+//       result ^= num;
 //     }
-
-//     return value;
-// };
-
-// console.log(singleNumber([1, 2, 2, 3, 3, 3, 4, 4, 1, 5]));
-
-
-function findSingleValue(arr) {
+    
+//     return result;
+//   }
+  
+//   const array = [1, 2, 2, 3, 3, 3, 4, 4, 1, 5];
+//   const singleValue = findSingleValue(array);
+  
+//   console.log(singleValue); // Output: 5
+  
+var singleNumber = function(nums) {
     let result = 0;
     
-    for (const num of arr) {
+    for (const num of nums) {
       result ^= num;
     }
     
     return result;
-  }
-  
-  const array = [1, 2, 2, 3, 3, 3, 4, 4, 1, 5];
-  const singleValue = findSingleValue(array);
-  
-  console.log(singleValue); // Output: 5
-  
+};
+
+
+console.log(singleNumber([2,2,1]));
+
+
