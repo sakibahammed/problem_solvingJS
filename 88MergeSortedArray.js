ce099ae57f9fbd846d6f6ed298b4a5b00184f28d
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-10-23 03:33:51 +0600

    buggedcode

--- 88MergeSortedArray.js
+++ 88MergeSortedArray.js
@@ -1,7 +1,22 @@
-var merge = function(nums1, m  , n){
- 
-   
-}
+var merge = function(nums1 , nums2) {
+    let array1 = []
+    for(let i = 0;i<nums1.length;i++){
+        if(nums1[i]>0){
+            array1.push(nums1[i]);
+        }
+    }
+
+    let array2 = []
+    for(let i=0;i<nums2.length;i++){
+        if(nums2[i]>0){
+            array2.push(nums2[i]);
+        } 
+    }
+
+    let sortedArray = ([...array1,...array2]);
+    console.log(sortedArray);
+   console.log(sortedArray.sort());
+};
 
 
-console.log(merge([1,2,3],3));
+console.log(merge([1,2,3,0,0,0],[2,5,6]));
